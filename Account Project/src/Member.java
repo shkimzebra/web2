ac0f6ee7169821944c35b022d3bc2ac6ad236bb6
user1 <user1@DESKTOP-0KMR2CV>
2022-06-13 16:07:34 +0900

    third commit

--- Account Project/src/Member.java
+++ Account Project/src/Member.java
@@ -3,7 +3,7 @@ public class Member {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Github Testing");
+		System.out.println("Git testing");
 
 	}
 
